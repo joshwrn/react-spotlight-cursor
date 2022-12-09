5416611253ef6ad76a4f6a9a093d573f67a14167
joshwrn <joshnwarren@gmail.com>
2022-12-09 00:14:01 -0800

    initial

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,222 @@
-import 'react-app-polyfill/ie11';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import "react-app-polyfill/ie11"
+import * as React from "react"
+
+import * as ReactDOM from "react-dom"
+import styled from "styled-components"
+
+import { SpotlightItem } from "../."
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #000000;
+  margin: 0;
+  padding: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+  z-index: -1;
+  gap: 50px;
+  color: #ffffff94;
+  font-family: sans-serif;
+  user-select: none;
+`
+
+const Demo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 500px;
+  height: 200px;
+  margin: 0;
+  padding: 0;
+  transition: border-color 0.5s ease-in-out;
+  border-radius: 20px;
+  border: 1px solid rgb(255, 255, 255, 0);
+  cursor: pointer;
+  overflow: hidden;
+  :hover {
+    border-color: #ffffff1f;
+  }
+`
+const ItemWrapper = styled.div`
+  display: flex;
+  overflow: hidden;
+  border-radius: 20px;
+`
+const ColorWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  > button {
+    cursor: pointer;
+    background-color: transparent;
+    border: none;
+    color: #ffffff65;
+    font-size: 14px;
+    font-weight: 500;
+    transition: color 0.5s ease-in-out;
+    border: 1px solid #ffffff2a;
+    cursor: pointer;
+    overflow: hidden;
+    padding: 10px 20px;
+    border-radius: 8px;
+    transition: color 0.5s ease-in-out, border-color 0.5s ease-in-out;
+    :hover {
+      color: #ffffff91;
+      border-color: #ffffff91;
+    }
+  }
+`
+const ColorContainer = styled.div`
+  display: flex;
+  gap: 0px;
+  transition: transform 0.5s ease-in-out;
+
+  :hover {
+    transform: scaleY(1.6);
+  }
+  input[type="color"] {
+    -webkit-appearance: none;
+    border: none;
+    width: 32px;
+    height: 32px;
+    opacity: 0;
+    -webkit-appearance: none;
+    cursor: pointer;
+  }
+  input[type="color"]::-webkit-color-swatch-wrapper {
+    padding: 0;
+    border: none;
+  }
+  > main {
+    height: 32px;
+    overflow-y: hidden;
+    border-radius: 20px;
+    position: absolute;
+    z-index: -1;
+    > div {
+      display: flex;
+      filter: blur(40px);
+    }
+  }
+`
+
+type Gradient = {
+  color1: string
+  color2: string
+  color3: string
+  color4: string
+  color5: string
+  color6: string
+  color7: string
+  color8: string
+  color9: string
+  color10: string
+  color11: string
+  color12: string
+  color13: string
+  color14: string
+}
+
+const randomColor = () => {
+  const letters = `0123456789ABCDEF`
+  let color = `#`
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
+}
 
 const App = () => {
-  return (
-    <div>
-      <Thing />
-    </div>
-  );
-};
+  const [gradient, setGradient] = React.useState({
+    color1: `#ffffff`,
+    color2: `#ffffff`,
+    color3: `#ffffff`,
+    color4: `#ffffff`,
+    color5: `#ff0000`,
+    color6: `#ff9a00`,
+    color7: `#d0de21`,
+    color8: `#4fdc4a`,
+    color9: `#3fdad8`,
+    color10: `#2fc9e2`,
+    color11: `#1c7fee`,
+    color12: `#5f15f2`,
+    color13: `#ba0cf8`,
+    color14: `#fb07d9`,
+  })
+  const g = `radial-gradient(
+    closest-side,
+    ${gradient.color1} 12.5%,
+    ${gradient.color2} 25%,
+    ${gradient.color3} 37.5%,
+    ${gradient.color4} 50%,
+    ${gradient.color5} 55%,
+    ${gradient.color6} 60%,
+    ${gradient.color7} 65%,
+    ${gradient.color8} 70%,
+    ${gradient.color9} 75%,
+    ${gradient.color10} 80%,
+    ${gradient.color11} 85%,
+    ${gradient.color12} 90%,
+    ${gradient.color13} 95%,
+    ${gradient.color14} 98%,
+    transparent 100%
+  )`
 
-ReactDOM.render(<App />, document.getElementById('root'));
+  const randomize = () => {
+    for (let i = 1; i < 15; i++) {
+      setGradient((prev) => ({
+        ...prev,
+        [`color${i}`]: randomColor(),
+      }))
+    }
+  }
+  return (
+    <Wrapper>
+      <ItemWrapper>
+        <SpotlightItem background={g} scaleOnTap={false}>
+          <Demo>Hover me</Demo>
+        </SpotlightItem>
+      </ItemWrapper>
+      <ColorWrapper>
+        <ColorContainer>
+          {Object.keys(gradient).map((key) => (
+            <input
+              key={key}
+              type="color"
+              value={gradient[key as keyof Gradient]}
+              onChange={(e) =>
+                setGradient({ ...gradient, [key]: e.target.value })
+              }
+            />
+          ))}
+          <main>
+            <div>
+              {Object.keys(gradient).map((key) => (
+                <div
+                  key={key}
+                  style={{
+                    width: `32px`,
+                    height: `32px`,
+                    backgroundColor: gradient[key as keyof Gradient],
+                  }}
+                />
+              ))}
+            </div>
+          </main>
+        </ColorContainer>
+        <button onClick={() => randomize()}>randomize</button>
+      </ColorWrapper>
+    </Wrapper>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById(`root`))
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,57 @@
-import * as React from 'react';
+import type { FC } from "react"
+import React, { useState } from "react"
 
-// Delete me
-export const Thing = () => {
-  return <div>Welcome to your first test package.</div>;
-};
+import type { MotionProps } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
+import type { FlattenInterpolation, ThemeProps } from "styled-components"
+import styled from "styled-components"
+
+import { StyledSpotLight, useSpotLight } from "./Components/Spotlight"
+
+export type CSSProps = FlattenInterpolation<ThemeProps<unknown>> | undefined
+
+const Wrapper = styled(motion.div)<{ css: CSSProps }>`
+  display: flex;
+  align-items: center;
+  transition: background-color 0.2s ease-in-out;
+  position: relative;
+  overflow: hidden;
+  :hover {
+    ${StyledSpotLight} {
+      opacity: 0.25;
+    }
+  }
+  ${({ css }) => css}
+`
+
+export const SpotlightItem: FC<
+  MotionProps &
+    React.HTMLAttributes<HTMLDivElement> & {
+      children: React.ReactNode
+      css?: CSSProps
+      opacity?: number
+      scaleOnTap?: boolean
+      background?: string
+    }
+> = ({ children, css, opacity, scaleOnTap, background, ...props }) => {
+  const [hover, setHover] = useState(false)
+  const { ref, handleMouse, SpotLight, setTap } = useSpotLight({
+    opacity,
+    scaleOnTap,
+    background,
+  })
+  return (
+    <Wrapper
+      ref={ref}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => (setHover(false), setTap(false))}
+      onMouseMove={handleMouse}
+      onMouseDown={() => setTap(true)}
+      css={css}
+      {...props}
+    >
+      {children}
+      <AnimatePresence>{hover && SpotLight}</AnimatePresence>
+    </Wrapper>
+  )
+}
